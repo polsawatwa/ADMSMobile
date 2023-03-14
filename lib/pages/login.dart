6b5c79312894543549798e1bec318af005cd94e1
kittipong2544 <63160288@go.buu.ac.th>
2023-03-14 21:35:23 +0700

    login by gun

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,6 +3,8 @@ import 'home.dart';
 import 'headfoot.dart';
 import 'welcome.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   // const LoginPage({super.key});
@@ -18,12 +20,11 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       color: Color(0xFF7286D3),
-
+      color: Color(0xFF7286D3),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
+          backgroundColor: Colors.transparent,
           body: ListView(children: [
-    Text(
+            Text(
               'Login',
               style: const TextStyle(
                 fontWeight: FontWeight.bold,
@@ -33,54 +34,102 @@ class _LoginPageState extends State<LoginPage> {
               ),
               textAlign: TextAlign.center,
             ),
-        Image.network(
-          'https://cdn-icons-png.flaticon.com/512/2170/2170153.png',
-          height: 300,
-          width: 300,
-        ),
-        
-    
-        SizedBox(height: 30),
-        TextField(
-            controller: username,
-            decoration: InputDecoration(
-               prefixIcon: Icon(Icons.account_circle),
-                labelText: 'Username', border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0),))),
-        SizedBox(height: 15),
-        TextField(
-            controller: password,
-            obscureText: true,
-            decoration: InputDecoration(
-         prefixIcon: Icon(Icons.key),
-                labelText: 'Password', border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0),))),
-        SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () {
-            if (username.text == 'super_admin' && password.text == 'Spa#1478') {
-              print("Username = super_admin and Password = Spa#1478");
-              setState(() {
-                username.text = 'super_admin';
-                password.text = 'Spa#1478';
-                setUsername(username.text);
-                setPassword(password.text);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => CurvePage()));
-              });
-            } else {
-              print("wrong username or password");
-               ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Wrong username or password.'),
-          backgroundColor: Colors.red,
-        ),
-      );
-            }
-            
-          },
-          child: Text('เข้าสู่ระบบ', style: TextStyle(fontFamily: 'Prompt')),
-          style: ButtonStyle(
-             fixedSize: MaterialStateProperty.all<Size>(
-      Size(200, 50),
-    ),shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            Image.network(
+              'https://cdn-icons-png.flaticon.com/512/2170/2170153.png',
+              height: 300,
+              width: 300,
+            ),
+            SizedBox(height: 30),
+            TextField(
+                controller: username,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.account_circle),
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ))),
+            SizedBox(height: 15),
+            TextField(
+                controller: password,
+                obscureText: true,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.key),
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ))),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () async {
+                // Send a GET request to the API endpoint to retrieve user data
+                var response = await http
+                    .get(Uri.parse('http://localhost:8000/api/user/'));
+                if (response.statusCode == 200) {
+                  // If the request is successful, parse the response body
+                  var data = jsonDecode(response.body);
+                  // Check if the entered username and password match any user in the API
+                  var user = data.firstWhere(
+                      (u) =>
+                          u['username'] == username.text &&
+                          u['password'] == password.text,
+                      orElse: () => null);
+                  if (user != null) {
+                    print('Login successful');
+                    setState(() {
+                      setUsername(username.text);
+                      setPassword(password.text);
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => CurvePage()));
+                    });
+                  } else {
+                    print('Invalid username or password');
+                  }
+                } else {
+                  print("wrong username or password");
+                  print('Failed to retrieve user data from the API');
+                }
+              },
+              child:
+                  Text('เข้าสู่ระบบ', style: TextStyle(fontFamily: 'Prompt')),
+              style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all<Size>(
+                    Size(200, 50),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  side: MaterialStateProperty.all<BorderSide>(
+                    BorderSide(
+                      width: 2,
+                      color: Color.fromARGB(255, 96, 125, 139),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(Colors.blueGrey),
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.fromLTRB(50, 20, 50, 20)),
+                  textStyle:
+                      MaterialStateProperty.all(TextStyle(fontSize: 15))),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WelcomePage()));
+              },
+              child: Text(
+                'ย้อนกลับ',
+                style: TextStyle(
+                  fontFamily: 'Prompt',
+                  fontSize: 20,
+                ),
+              ),
+              style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all<Size>(
+                  Size(200, 50),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
@@ -88,54 +137,21 @@ class _LoginPageState extends State<LoginPage> {
                 side: MaterialStateProperty.all<BorderSide>(
                   BorderSide(
                     width: 2,
-                    color: Color.fromARGB(255, 96, 125,139),
+                    color: Color.fromARGB(255, 96, 125, 139),
                   ),
                 ),
-              backgroundColor: MaterialStateProperty.all(Colors.blueGrey),
-              padding: MaterialStateProperty.all(
-                  EdgeInsets.fromLTRB(50, 20, 50, 20)),
-              textStyle: MaterialStateProperty.all(TextStyle(fontSize: 15))),
-        ),
-         SizedBox(height: 15),
-       ElevatedButton(
-  onPressed: () {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => WelcomePage()));
-  },
-  child: Text(
-    'ย้อนกลับ',
-    style: TextStyle(
-      fontFamily: 'Prompt',
-      fontSize: 20,
-    ),
-  ),
-  style: ButtonStyle(
-    fixedSize: MaterialStateProperty.all<Size>(
-      Size(200, 50),
-    ),
-    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-      RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
-      ),
-    ),
-    side: MaterialStateProperty.all<BorderSide>(
-      BorderSide(
-        width: 2,
-        color: Color.fromARGB(255, 96, 125, 139),
-      ),
-    ),
-    backgroundColor: MaterialStateProperty.all(Colors.blueGrey),
-    padding: MaterialStateProperty.all(
-      EdgeInsets.fromLTRB(50, 20, 50, 20),
-    ),
-    textStyle: MaterialStateProperty.all(
-      TextStyle(
-        fontSize: 20,
-      ),
-    ),
-  ),
-)
-
-      ])),
+                backgroundColor: MaterialStateProperty.all(Colors.blueGrey),
+                padding: MaterialStateProperty.all(
+                  EdgeInsets.fromLTRB(50, 20, 50, 20),
+                ),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            )
+          ])),
     );
   }
 
