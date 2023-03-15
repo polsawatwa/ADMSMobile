e9aab1163fdcfc6ca1124fdc536efa8bc0f9c902
Naphawan Chaichuen <63160243@go.buu.ac.th>
2023-03-16 00:33:12 +0700

    แก้หัวภาพ

--- lib/pages/collection.dart
+++ lib/pages/collection.dart
@@ -7,6 +7,7 @@ import 'detail.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
 enum PopupItem { update, delete }
 
